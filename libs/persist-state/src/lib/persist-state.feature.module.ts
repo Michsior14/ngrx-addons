3c35ff7f25fe2ee4b3a94b48482b31c66895b954
Michał Mrozek <github@michalmrozek.dev>
2023-05-11 20:55:32 +0200

    fix: provide default type in feature modules (#179)
    Resolves #178

--- libs/persist-state/src/lib/persist-state.feature.module.ts
+++ libs/persist-state/src/lib/persist-state.feature.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PersistStateFeature } from './persist-state.feature';
 
 @NgModule()
-export class PersistStateFeatureModule<T> {
+export class PersistStateFeatureModule<T = object> {
   constructor(state: PersistStateFeature<T>) {
     state.addFeature();
   }
--- libs/sync-state/src/lib/sync-state.feature.module.ts
+++ libs/sync-state/src/lib/sync-state.feature.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { SyncStateFeature } from './sync-state.feature';
 
 @NgModule()
-export class SyncStateFeatureModule<T> {
+export class SyncStateFeatureModule<T = object> {
   constructor(state: SyncStateFeature<T>) {
     state.addFeature();
   }
